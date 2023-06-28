1d7ea882f739640831971feff792977664f85684
Hoang Son <hoangvansonit@gmail.com>
2023-06-28 23:21:23 +0700

    add sign up UI

--- app/login/page.js
+++ app/login/page.js
@@ -3,28 +3,37 @@
 import Image from 'next/image';
 import styles from './styles.module.css';
 import { BsFacebook } from 'react-icons/bs';
-import { IoMdEyeOff, IoMdEye, IoIosWarning } from 'react-icons/io';
+import {
+  IoMdEyeOff,
+  IoMdEye,
+  IoIosWarning,
+  IoIosArrowBack,
+} from 'react-icons/io';
+import { RiErrorWarningFill } from 'react-icons/ri';
 import { useState } from 'react';
 import Link from 'next/link';
 import * as Yup from 'yup';
 
 const validationSchema = Yup.object().shape({
-  email: Yup.string().required('Hãy nhập tên người dùng hoặc email'),
+  username: Yup.string().required('Hãy nhập tên người dùng hoặc email'),
   password: Yup.string().required('Hãy nhập mật khẩu'),
+  email: Yup.string().required('Hãy nhập email'),
 });
 
 export default function LoginPage() {
   const [showPassword, setShowPassword] = useState(false);
   const [remember, setRemember] = useState(false);
-  const [email, setEmail] = useState('');
+  const [username, setUsername] = useState('');
   const [password, setPassword] = useState('');
+  const [emailForGet, setEmailForget] = useState('');
   const [errors, setErrors] = useState('');
+  const [isForget, setIsForget] = useState(false);
 
   const handleLogin = async e => {
     e.preventDefault();
     try {
       await validationSchema.validate(
-        { email, password },
+        { username, password },
         { abortEarly: false },
       );
       // Perform login
@@ -33,7 +42,22 @@ export default function LoginPage() {
       err.inner.forEach(error => {
         validationErrors[error.path] = error.message;
       });
+      validationErrors.message =
+        'Email, tên đăng nhập hoặc mật khẩu không đúng.';
       setErrors(validationErrors);
+    }
+  };
+
+  const handleForget = async () => {
+    e.preventDefault();
+    try {
+      await validationSchema.validate(
+        { email: emailForGet },
+        { abortEarly: false },
+      );
+      // Perform login
+    } catch (err) {
+      setErrors({ ...errors, email: err.message });
     }
   };
 
@@ -42,6 +66,13 @@ export default function LoginPage() {
       <div className={styles['login-main']}>
         <div className={styles['login-container']}>
           <div className={`${styles['login-logo']}`}>
+            {isForget && (
+              <IoIosArrowBack
+                size={20}
+                className={styles['icon-back']}
+                onClick={() => setIsForget(false)}
+              />
+            )}
             <Image
               src='https://www.f-cdn.com/assets/main/en/assets/freelancer-logo.svg'
               alt='logo'
@@ -49,117 +80,176 @@ export default function LoginPage() {
               height={48}
             />
           </div>
-          <div className={styles['login-session']}>
-            <h2>Chào mừng trở lại</h2>
-            <Link href={'#'} className={styles['login-fb']}>
-              <BsFacebook size={23} />
-              Đăng nhập bằng Facebook
-            </Link>
-            <div className={styles['line-text']}>
-              <div className={styles['line']}></div>
-              <div className={styles['text']}>HOẶC</div>
-              <div className={styles['line']}></div>
-            </div>
-
-            {/* form login  */}
-            <form className={styles['login-form']} onSubmit={handleLogin}>
-              <div style={{ minHeight: '71px', marginBottom: '8px' }}>
-                <input
-                  type='text'
-                  placeholder='Email hoặc tên đăng nhập'
-                  onChange={e => setEmail(e.target.value)}
-                  className={errors.email ? styles['input-error'] : ''}
-                />
-
-                <p className={styles['input-validate-error']}>
-                  {errors.email ? (
-                    <>
-                      <IoIosWarning style={{ marginRight: '7px' }} />
-                      {errors.email}
-                    </>
-                  ) : (
-                    'error'
-                  )}
-                </p>
-              </div>
-              <div>
-                <div className={styles['input-password']}>
+          {isForget ? (
+            <div className={styles['login-session']}>
+              <h2 className='mb-3'>Tạo lại mật khẩu của bạn</h2>
+              <p className='text-center' style={{ fontSize: '14px' }}>
+                Nhập địa chỉ email Freelancer.com của bạn để chúng tôi có thể
+                tạo lại mật khẩu cho bạn.
+              </p>
+              <form className={styles['login-form']} onSubmit={handleForget}>
+                <div>
                   <input
-                    type={showPassword ? 'text' : 'password'}
-                    placeholder='Mật khẩu'
-                    onChange={e => setPassword(e.target.value)}
+                    type='email'
+                    placeholder='Nhập email'
+                    className={errors.email ? styles['input-error'] : ''}
+                    onChange={e => setEmailForget(e.target.value)}
                   />
-                  <div
-                    className={styles['show-password']}
-                    onClick={() => setShowPassword(!showPassword)}
+                  <p
+                    className={styles['input-validate-error']}
+                    style={{
+                      visibility: errors.email ? 'visible' : 'hidden',
+                    }}
                   >
-                    {!showPassword ? (
-                      <IoMdEyeOff size={25} />
-                    ) : (
-                      <IoMdEye size={25} />
-                    )}
-                  </div>
+                    <IoIosWarning style={{ marginRight: '7px' }} />
+                    {errors.email}
+                  </p>
                 </div>
-                <p className={styles['input-validate-error']}>
-                  {errors.password ? (
-                    <>
-                      <IoIosWarning style={{ marginRight: '7px' }} />
-                      {errors.password}
-                    </>
-                  ) : (
-                    ' k '
-                  )}
-                </p>
-              </div>
-
-              {/* remember me and forget password */}
-              <div className={styles['remember-forget']}>
-                <label>
-                  <input
-                    type='checkbox'
-                    checked={remember}
-                    onChange={() => setRemember(!remember)}
-                  />
-                  <span>Ghi nhớ tôi</span>
-                </label>
-                <p>Quên mật khẩu?</p>
-              </div>
-              <button>Đăng nhập</button>
-            </form>
-            <hr />
-            <p
-              style={{
-                fontSize: '13px',
-                textAlign: 'center',
-                marginBottom: '24px',
-              }}
-            >
-              Bạn chưa có tài khoản?{' '}
-              <Link href={'/signup'} style={{ textDecoration: 'none' }}>
-                Đăng ký
-              </Link>
-            </p>
-
-            {/* download on store */}
-            <div className='d-flex justify-content-around'>
-              <Link href={'#'}>
-                <Image
-                  src='/images/apple-app-store-badge.webp'
-                  width='131'
-                  height='44'
-                  alt='logo appstore'
-                />
-              </Link>
-              <Link href={'#'}>
-                <Image
-                  src='/images/google-play-store-badge.webp'
-                  width='131'
-                  height='44'
-                  alt='logo appstore'
-                />
-              </Link>
+                <button type='submit'>Tiếp Theo</button>
+              </form>
             </div>
-          </div>
+          ) : (
+            <div className={styles['login-session']}>
+              <h2>Chào mừng trở lại</h2>
+              <Link href={'#'} className={styles['login-fb']}>
+                <BsFacebook size={23} />
+                Đăng nhập bằng Facebook
+              </Link>
+              <div className={styles['line-text']}>
+                <div className={styles['line']}></div>
+                <div className={styles['text']}>HOẶC</div>
+                <div className={styles['line']}></div>
+              </div>
+
+              {/* form login  */}
+              <form className={styles['login-form']} onSubmit={handleLogin}>
+                <div>
+                  <input
+                    type='text'
+                    placeholder='Email hoặc tên đăng nhập'
+                    onChange={e => {
+                      setUsername(e.target.value);
+                      setErrors(prev => {
+                        return { ...prev, username: '' };
+                      });
+                    }}
+                    className={errors.username ? styles['input-error'] : ''}
+                  />
+
+                  <p
+                    className={styles['input-validate-error']}
+                    style={{
+                      visibility: errors.username ? 'visible' : 'hidden',
+                    }}
+                  >
+                    <IoIosWarning style={{ marginRight: '7px' }} />
+                    {errors.username}
+                  </p>
+                </div>
+                <div>
+                  <div
+                    className={`${styles['input-password']} ${
+                      errors.password ? styles['input-error'] : ''
+                    }`}
+                  >
+                    <input
+                      type={showPassword ? 'text' : 'password'}
+                      placeholder='Mật khẩu'
+                      onChange={e => {
+                        setPassword(e.target.value);
+                        setErrors(prev => {
+                          return { ...prev, password: '' };
+                        });
+                      }}
+                    />
+                    <div
+                      className={styles['show-password']}
+                      onClick={() => setShowPassword(!showPassword)}
+                    >
+                      {!showPassword ? (
+                        <IoMdEyeOff size={25} />
+                      ) : (
+                        <IoMdEye size={25} />
+                      )}
+                    </div>
+                  </div>
+                  <p
+                    className={styles['input-validate-error']}
+                    style={{
+                      visibility: errors.password ? 'visible' : 'hidden',
+                    }}
+                  >
+                    <IoIosWarning style={{ marginRight: '7px' }} />
+                    {errors.password}
+                  </p>
+                </div>
+
+                {/* remember me and forget password */}
+                <div className={styles['remember-forget']}>
+                  <label>
+                    <input
+                      type='checkbox'
+                      checked={remember}
+                      onChange={() => setRemember(!remember)}
+                    />
+                    <span>Ghi nhớ tôi</span>
+                  </label>
+                  <p onClick={() => setIsForget(true)}>Quên mật khẩu?</p>
+                </div>
+                {errors.message && (
+                  <div className={styles['error-message']}>
+                    <RiErrorWarningFill
+                      color='#eb3730'
+                      size={40}
+                      style={{ marginTop: '-7px ' }}
+                    />
+                    <p>{errors.message}</p>
+                    <span
+                      className={styles['button-close']}
+                      onClick={() => setErrors({ ...errors, message: '' })}
+                    >
+                      &#10006;
+                    </span>
+                  </div>
+                )}
+
+                <button type='submit'>Đăng nhập</button>
+              </form>
+              <hr />
+              <p
+                style={{
+                  fontSize: '13px',
+                  textAlign: 'center',
+                  marginBottom: '24px',
+                }}
+              >
+                Bạn chưa có tài khoản?{' '}
+                <Link href={'/signup'} style={{ textDecoration: 'none' }}>
+                  Đăng ký
+                </Link>
+              </p>
+
+              {/* download on store */}
+              <div className='d-flex justify-content-around'>
+                <Link href={'#'}>
+                  <Image
+                    src='/images/apple-app-store-badge.webp'
+                    width='131'
+                    height='44'
+                    alt='logo appstore'
+                  />
+                </Link>
+                <Link href={'#'}>
+                  <Image
+                    src='/images/google-play-store-badge.webp'
+                    width='131'
+                    height='44'
+                    alt='logo appstore'
+                  />
+                </Link>
+              </div>
+            </div>
+          )}
         </div>
       </div>
     </div>
--- app/signup/page.js
+++ app/signup/page.js
@@ -1,7 +1,186 @@
-import React from 'react'
+'use client';
 
-export default function SignupPage() {
-    return (
-        <div>SignupPage</div>
-    )
+import Image from 'next/image';
+import styles from './styles.module.css';
+import { BsFacebook } from 'react-icons/bs';
+import { IoMdEyeOff, IoMdEye, IoIosWarning } from 'react-icons/io';
+import { RiErrorWarningFill } from 'react-icons/ri';
+import { useState } from 'react';
+import Link from 'next/link';
+import * as Yup from 'yup';
+
+const validationSchema = Yup.object().shape({
+  password: Yup.string()
+    .min(6, 'Phải dài tối thiểu 6 ký tự')
+    .required('Hãy nhập mật khẩu'),
+  email: Yup.string()
+    .email('Hãy nhập địa chỉ email hợp lệ')
+    .required('Hãy nhập địa chỉ email'),
+});
+
+export default function LoginPage() {
+  const [showPassword, setShowPassword] = useState(false);
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [errors, setErrors] = useState('');
+  const [isAgree, setIsAgree] = useState(false);
+
+  const handleLogin = async e => {
+    e.preventDefault();
+    try {
+      await validationSchema.validate(
+        { email, password },
+        { abortEarly: false },
+      );
+      // Perform login
+    } catch (err) {
+      const validationErrors = {};
+      err.inner.forEach(error => {
+        validationErrors[error.path] = error.message;
+      });
+      setErrors(validationErrors);
+    }
+  };
+
+  return (
+    <div className={styles['login-page']}>
+      <div className={styles['login-main']}>
+        <div className={styles['login-container']}>
+          <div className={`${styles['login-logo']}`}>
+            <Image
+              src='https://www.f-cdn.com/assets/main/en/assets/freelancer-logo.svg'
+              alt='logo'
+              width={199}
+              height={48}
+            />
+          </div>
+
+          <div className={styles['login-session']}>
+            <h2>Đăng ký</h2>
+            <Link href={'#'} className={styles['login-fb']}>
+              <BsFacebook size={23} />
+              Tiếp tục với Facebook
+            </Link>
+            <div className={styles['line-text']}>
+              <div className={styles['line']}></div>
+              <div className={styles['text']}>HOẶC</div>
+              <div className={styles['line']}></div>
+            </div>
+
+            {/* form login  */}
+            <form className={styles['login-form']} onSubmit={handleLogin}>
+              <div>
+                <input
+                  type='text'
+                  placeholder='Email hoặc tên đăng nhập'
+                  onChange={e => {
+                    setEmail(e.target.value);
+                    setErrors(prev => {
+                      return { ...prev, email: '' };
+                    });
+                  }}
+                  className={errors.email ? styles['input-error'] : ''}
+                />
+
+                <p
+                  className={styles['input-validate-error']}
+                  style={{
+                    visibility: errors.email ? 'visible' : 'hidden',
+                  }}
+                >
+                  <IoIosWarning style={{ marginRight: '7px' }} />
+                  {errors.email}
+                </p>
+              </div>
+              <div>
+                <div
+                  className={`${styles['input-password']} ${
+                    errors.password ? styles['input-error'] : ''
+                  }`}
+                >
+                  <input
+                    type={showPassword ? 'text' : 'password'}
+                    placeholder='Mật khẩu'
+                    onChange={e => {
+                      setPassword(e.target.value);
+                      setErrors(prev => {
+                        return { ...prev, password: '' };
+                      });
+                    }}
+                  />
+                  <div
+                    className={styles['show-password']}
+                    onClick={() => setShowPassword(!showPassword)}
+                  >
+                    {!showPassword ? (
+                      <IoMdEyeOff size={25} />
+                    ) : (
+                      <IoMdEye size={25} />
+                    )}
+                  </div>
+                </div>
+                <p
+                  className={styles['input-validate-error']}
+                  style={{
+                    visibility: errors.password ? 'visible' : 'hidden',
+                  }}
+                >
+                  <IoIosWarning style={{ marginRight: '7px' }} />
+                  {errors.password}
+                </p>
+              </div>
+
+              {/* remember me and forget password */}
+              <div className={styles['remember-forget']}>
+                <label>
+                  <input
+                    type='checkbox'
+                    checked={isAgree}
+                    onChange={() => setIsAgree(!isAgree)}
+                  />
+                  <span className='text-danger' style={{ fontSize: '13px' }}>
+                    Tôi đồng ý với{' '}
+                    <Link href={'/about/terms'}>Thỏa thuận Người Dùng</Link> và{' '}
+                    <Link href={'/about/privacy'}>Chính Sách Riêng Tư</Link> của
+                    Freelancer
+                  </span>
+                </label>
+              </div>
+              {errors.message && (
+                <div className={styles['error-message']}>
+                  <RiErrorWarningFill
+                    color='#eb3730'
+                    size={40}
+                    style={{ marginTop: '-7px ' }}
+                  />
+                  <p>{errors.message}</p>
+                  <span
+                    className={styles['button-close']}
+                    onClick={() => setErrors({ ...errors, message: '' })}
+                  >
+                    &#10006;
+                  </span>
+                </div>
+              )}
+
+              <button type='submit'>Tham gia Freelancer</button>
+            </form>
+            <hr />
+            <p
+              style={{
+                fontSize: '13px',
+                textAlign: 'center',
+                marginBottom: '24px',
+              }}
+            >
+              Bạn đã có tài khoản?{' '}
+              <Link href={'/login'} style={{ textDecoration: 'none' }}>
+                Đăng nhập
+              </Link>
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
