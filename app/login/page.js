1c5489c079719065b3e479e22282843fc158f61f
Hoang Son <hoangvansonit@gmail.com>
2023-06-28 13:35:24 +0700

    add login page ui

--- app/login/page.js
+++ app/login/page.js
@@ -1,7 +1,19 @@
-import React from 'react'
+import Image from 'next/image';
+import styles from './style.module.css';
 
 export default function LoginPage() {
-    return (
-        <div>LoginPage</div>
-    )
+  return (
+    <div className={styles['login-page']}>
+      <div className={styles['login-container']}>
+        <div className={styles['login-form']}>
+          <Image
+            src='https://www.f-cdn.com/assets/main/en/assets/freelancer-logo.svg'
+            alt='logo'
+            width={199}
+            height={48}
+          />
+        </div>
+      </div>
+    </div>
+  );
 }
