f69893248534c5c55e959f4448a8c3be0e91a0de
zukatahara <quyenpro7a@gmail.com>
2023-06-29 14:07:07 +0700

    update app router

--- app/layout.js
+++ app/layout.js
@@ -19,9 +19,9 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={inter.className}>
-        <Header />
+        <Header pathname={pathname}/>
         {children}
-        <Footer />
+        <Footer pathname={pathname}/>
       </body>
     </html>
   );
